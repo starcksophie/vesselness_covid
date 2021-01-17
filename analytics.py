6f5fd7b8977455a50da16fbb0023b8c8f1248acf
Sophie Starck <sophie.starck@epita.fr>
2021-01-17 15:30:50 +0100

    aded component count

--- analytics.py
+++ analytics.py
@@ -27,23 +27,20 @@ def distance(seg, verbose=False):
 
 
 def label_value(dist):
-    label_map = label(dist)
+    label_map, label_nbr = label(dist)
     label_map = label_map.flatten()
     flat_dist = dist.flatten()
-    label_nbr = max(label_map)
     dist_per_label = np.array([np.array([
         flat_dist[i]  for i in range(len(flat_dist)) if label_map[i] == n ])
                         for n in range(1, label_nbr)])
-    mean_ = np.array([a.mean() for a in dist_per_label])
-    max_ = np.array([a.max() for a in dist_per_label])
-    try:
-        result["mean_mean_all_vessel"] = mean_.mean()
-        result["max_max_all_vessel"] = max_.max()
-        result["mean_max_all_vessel"] = max_.mean()
-        result["mean_all_vessel"] = mean_
-        result["max_all_vessel"] = max_
-    except:
-        print("error")
+    mean_ = [a.mean() for a in dist_per_label]
+    max_ = [a.max() for a in dist_per_label]
+    result["mean_mean_all_vessel"] = mean_.mean()
+    result["max_max_all_vessel"] = max_.max()
+    result["mean_max_all_vessel"] = max_.mean()
+    result["mean_all_vessel"] = mean_
+    result["max_all_vessel"] = max_
+    result["component_count"] = label_nbr
     return mean_, max_
     
 #def skeleton_dist(skeleton):
